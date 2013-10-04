f56a79b7710b6c73410aa2b2d4f2bb115004e91e
matevz <matevz.jekovec@gmail.com>
2013-10-04 12:34:13 +0000

    Poprevljen LOG.

--- tools/nightly-wrapper.sh
+++ tools/nightly-wrapper.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ODSTOOLS_PATH=/home/mjekovec/odstools
-LOG=$OUT_PATH/ods-r${$1}.log
+LOG=/var/www/sl.opendatastructures.org/versions-sl/ods-r${$1}.log
 
 $ODSTOOLS_PATH/nightly.sh $1 $2 &2>$LOG &

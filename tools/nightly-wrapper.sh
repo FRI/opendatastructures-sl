84e5b62025831f678249d11e8011da2644ecbba1
matevz <matevz.jekovec@gmail.com>
2013-10-04 12:52:13 +0000

    Fixed pipelining.

--- tools/nightly-wrapper.sh
+++ tools/nightly-wrapper.sh
@@ -3,4 +3,4 @@
 ODSTOOLS_PATH=/home/mjekovec/odstools
 LOG=/var/www/sl.opendatastructures.org/versions-sl/ods-r${1}.log
 
-$ODSTOOLS_PATH/nightly.sh $1 $2 &2>$LOG &
+$ODSTOOLS_PATH/nightly.sh $1 $2 2>&1>$LOG &

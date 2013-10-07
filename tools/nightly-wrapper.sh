5b16c5d9916734af598a4046da31088717be7e53
matevz <matevz.jekovec@gmail.com>
2013-10-07 10:08:23 +0000

    nightly-wrapper ne potrebuje več revision-a.

--- tools/nightly-wrapper.sh
+++ tools/nightly-wrapper.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 LOG=/var/www/sl.opendatastructures.org/versions-sl/ods-r${1}.log
+REV=`svn info https://lusy.fri.uni-lj.si/svn/ods/sl --username ods-nightly --password y4FV\&X8C6 --non-interactive --trust-server-cert | grep Revision | awk -F": " '{print $2}'`
 
 cd `dirname $0`
-./nightly.sh $1 $2 2>&1>$LOG &
+./nightly.sh $REV 2>&1>$LOG &

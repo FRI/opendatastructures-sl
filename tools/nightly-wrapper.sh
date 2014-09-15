bdb5f6c97f21305694cead900cbd4357a1f180f6
matevz <matevz.jekovec@gmail.com>
2014-09-15 08:47:45 +0000

    Počiščena korenska mapa.

--- tools/nightly-wrapper.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-REV=`svn info https://lusy.fri.uni-lj.si/svn/ods/sl --username ods-nightly --password y4FV\&X8C6 --non-interactive --trust-server-cert | grep Revision | awk -F": " '{print $2}'`
-LOG=/var/www/sl.opendatastructures.org/versions-sl/ods-sl-r${REV}.log
-
-cd `dirname $0`
-./nightly.sh $REV 2>&1>>$LOG
